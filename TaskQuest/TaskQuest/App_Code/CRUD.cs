5942c0eecbb10eb942ec5fe8d610c987abdde3ac
Emanuel Miyagawa <miyagawa.emanuel@gmail.com>
2017-05-10 13:40:34 -0300

    Update CRUD.cs

--- TaskQuest/TaskQuest/App_Code/CRUD.cs
+++ TaskQuest/TaskQuest/App_Code/CRUD.cs
@@ -1,8 +1,14 @@
 using TaskQuest.Models;
+using TaskQuest.App_Code;
 using System;
 
 namespace TaskQuest.App_Code{
     public static class CRUD{
+        
+        public void Create(){}
+        public void Read(){}
+        public void Update(){}
+        public void Delete(){}
         
     }
 }
