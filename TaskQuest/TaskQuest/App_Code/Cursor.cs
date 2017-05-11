cfd41fa826486b102b5d9b42fc6014db3065643e
Emanuel Miyagawa <miyagawa.emanuel@gmail.com>
2017-05-11 17:55:22 -0300

    Update Cursor.cs

--- TaskQuest/TaskQuest/App_Code/Cursor.cs
+++ TaskQuest/TaskQuest/App_Code/Cursor.cs
@@ -4,38 +4,29 @@ using MySql.Data.MySqlClient;
 
 namespace TaskQuest.App_Code
 {
-    private var connection = new MySqlConnection(ConfigurationManager.AppSettings["DefaultConnection"]);
-    var command = new conenction.CreateCommand();
-    
-    public static class AddQuery(string query)
-    {
-        command.CommandText = query;
-    }
-    
-    public static class AddParameter(string parametro, object valor)
-    {
-        command.Parameters.Add(new MySqlParameter(nomeDoParametro, valor););
-    }
-    
-    public static class Execute()
-    {
-        
-    }
-    
-    public static class Select()
-    {
-    
-    }
+    public static class Cursor{
+        private var connection = new MySqlConnection(ConfigurationManager.AppSettings["DefaultConnection"]).open();
+        private var command = new conenction.CreateCommand();
+        private var adapter =  new MySqlDataAdapter();
 
-    public static class Cursor
-    {
-        
-        public static IDataAdapter Adapter(IDbCommand command)
+        public static class AddQuery(string query)
         {
-            IDbDataAdapter adap = new MySqlDataAdapter();
-            adap.SelectCommand = command;
-            return adap;
+            command.CommandText = query;
         }
 
+        public static class AddParameter(string parametro, object valor)
+        {
+            command.Parameters.Add(new MySqlParameter(nomeDoParametro, valor););
+        }
+        
+        public static class Execute()
+        {
+
+        }
+
+        public static class Select()
+        {
+
+        }
     }
 }
