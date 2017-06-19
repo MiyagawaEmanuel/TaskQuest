41a08f5f0911b87aa4c4e7b50b0b4fd96cd3d319
Emanuel Miyagawa <miyagawa.emanuel@gmail.com>
2017-06-19 17:42:00 -0300

    Atualizando ConfiguracaoViewModel

---
+++ TaskQuest/Models/ViewModels/ConfiguracaoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskQuest.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        User usuario = new User();
+
+        public List<Cartao> Cartoes = new List<Cartao>();
+
+        public List<Telefone> Telefones = new List<Telefone>();
+
+        public TelefoneViewModel telefoneViewModel = new TelefoneViewModel();
+
+        public CartaoViewModel cartaoViewModel = new CartaoViewModel();
+
+    }
+}
