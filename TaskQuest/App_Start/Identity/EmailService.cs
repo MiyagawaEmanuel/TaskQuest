b66e76de49827db01f3565626219d665b775d10e
emanuelhm <miyagawa.emanuel@gmail.com>
2017-09-16 14:44:45 -0300

    Finalizando erro 404 customizado

--- TaskQuest/App_Start/Identity/EmailService.cs
+++ TaskQuest/App_Start/Identity/EmailService.cs
@@ -19,22 +19,17 @@ namespace TaskQuest.Identity
         // Implementação de e-mail manual
         private Task SendMail(IdentityMessage message)
         {
-
-            var text = HttpUtility.HtmlEncode(message.Body);
-
             var msg = new MailMessage();
             msg.From = new MailAddress("taskquest01@gmail.com", "TaskQuest");
             msg.To.Add(new MailAddress(message.Destination));
             msg.Subject = message.Subject;
-            msg.AlternateViews.Add(
-                AlternateView.CreateAlternateViewFromString(text, System.Text.Encoding.UTF8, MediaTypeNames.Text.Html));
-
+            msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(message.Body, null, "text/html"));
+            msg.IsBodyHtml = true;
             var smtpClient = new SmtpClient("smtp.gmail.com", Convert.ToInt32(587));
             var credentials = new NetworkCredential("taskquest01@gmail.com", "Teste@123");
             smtpClient.Credentials = credentials;
             smtpClient.EnableSsl = true;
             smtpClient.Send(msg);
-
 
             return Task.FromResult(0);
         }
